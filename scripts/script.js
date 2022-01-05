9ab4a64ecdddda1e86ca9d9c5590dd473c971eca
Iwantroca <mohitnotiyal@yahoo.com>
2022-01-05 10:18:22 +0530

    Add working logic to add icons to array

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,74 @@
-const GameBoard = () => {
+const GameBoard = (() => {
+  let round = 1;
   const board = new Array(9).fill("");
-  return { board };
-};
+  const getIndex = (e) => {
+    let index = e.target.dataset.boxIndex;
+    console.log(index);
+    return index;
+  };
+  const setIndex = (e) => {
+    GameBoard.board[getIndex(e)] = PlayerController.getCurrentPlayer().marker;
+    console.log(GameBoard.board);
+  };
+  return { round, board, getIndex, setIndex };
+})();
 
 const Player = (marker) => {
-  return { marker };
+  const activePlayer = false;
+  return { marker, activePlayer };
 };
-let ticTacToe = GameBoard();
-let playerOne = Player("O");
+
+const PlayerController = (() => {
+  let playerO = Player("O");
+  let playerX = Player("X");
+  let currentPlayer;
+  let switchPlayer = () => {
+    if (currentPlayer == undefined || !playerO.activePlayer) {
+      playerO.activePlayer = true;
+      playerX.activePlayer = false;
+      currentPlayer = playerO;
+    } else if (playerO.activePlayer && !playerX.activePlayer) {
+      playerX.activePlayer = true;
+      playerO.activePlayer = false;
+      currentPlayer = playerX;
+    }
+  };
+
+  return {
+    playerO,
+    playerX,
+    switchPlayer,
+    getCurrentPlayer: () => currentPlayer,
+  };
+})();
+
+const newGameController = (() => {
+  const updateRound = () => {
+    GameBoard.round += 1;
+    console.log(GameBoard.round);
+  };
+  return { updateRound };
+})();
 
 // add event listener to each box
-let boxes = document.querySelectorAll(".board-box");
 // 1. to get its index in array
 //    -> to get the index add data-Index to html
 // 2. and then check for player's turn
 //    -> Start with O's turn and keep switching turn
 // 3. as per player's turn insert that marker in the array as per the fetched index.
 //    -> ticTacToe.board[index] = playerWithTurn.marker;
-ticTacToe.board[3] = playerOne.marker;
-console.log(ticTacToe.board);
+
+const GameController = (() => {
+  const updateGame = () => {
+    let boxes = document.querySelectorAll(".board-box");
+    [...boxes].forEach((x) => {
+      x.addEventListener("click", (e) => {
+        PlayerController.switchPlayer();
+        GameBoard.getIndex(e);
+        GameBoard.setIndex(e);
+        console.log(GameBoard.board);
+      });
+    });
+  };
+  return { updateGame };
+})();
